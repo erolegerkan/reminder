0ddfb01af36a9736dddf8206a8fa9cb4e56357e5
erolegerkan <erolege.education@hotmail.com>
2026-01-16 20:56:36 +0300

    feat: Add navigation for RAppBar

--- lib/core/widgets/r_appbar.dart
+++ lib/core/widgets/r_appbar.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:reminder/core/constants/enums/screen_enum.dart';
 import 'package:reminder/core/design/r_colors.dart';
+import 'package:reminder/core/providers/screen_provider.dart';
 import 'package:reminder/core/widgets/r_icon.dart';
 import 'package:reminder/core/widgets/r_text.dart';
 
@@ -9,7 +12,18 @@ class RAppBar extends StatelessWidget implements PreferredSizeWidget {
   @override
   Widget build(BuildContext context) {
     return AppBar(
-      title: RText(textData: "reminder", fontWeight: FontWeight.w500,fontSize: 25,),
+      title: Consumer<ScreenProvider>(
+        builder: (context, serviceProviderModel, child) => InkWell(
+          onLongPress: () {
+            serviceProviderModel.changeScreenStatus(ReminderScreens.home);
+          },
+          child: RText(
+            textData: "reminder",
+            fontWeight: FontWeight.w500,
+            fontSize: 25,
+          ),
+        ),
+      ),
       backgroundColor: RColors.darkCard,
       iconTheme: IconThemeData(color: RColors.darkTitle),
       actions: [
